fd85fc574b98d83d36595dee163cb250da996412
Tim Ninan <tim.ninan@gmail.com>
2017-09-12 10:09:13 -0500

    Test Commit #1

--- src/underbar.js
+++ src/underbar.js
@@ -31,7 +31,7 @@
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
-    return n === undefined ? array[0] : array.slice(0, n);
+    return n === undefined ? array[0] : array.slice(1, n);
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
