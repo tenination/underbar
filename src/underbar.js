4f6bef03517771c729ba5f667ee03ff7328d4cc6
Tim Ninan <tim.ninan@gmail.com>
2017-09-13 18:31:41 -0500

    solved 'map'

--- src/underbar.js
+++ src/underbar.js
@@ -115,6 +115,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var output = [];
+    for (var i = 0; i < collection.length; i++) {
+        output.push(iterator(collection[i]));
+      }
+    return output; 
   };
 
   /*
